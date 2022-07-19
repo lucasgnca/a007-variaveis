9dab69c6f5a36a5b76435182e24fb15f12b633ad
Lucas <lc.lucas070@gmail.com>
2022-07-19 01:35:51 -0300

    Resolucao dos exercicios da aula 007 da turma barbosa-d

---
+++ exercicio2/script.js
@@ -0,0 +1,26 @@
+//criando perguntas em prompt
+let pergunta1 = prompt('Você bebeu água hoje?');
+let pergunta2 = prompt('Você comeu pão hoje?');
+let pergunta3 = prompt('Sua camisa é vermelha?');
+
+//colocando respostas em variáveis
+let resposta1 = pergunta1;
+let resposta2 = pergunta2;
+let resposta3 = pergunta3;
+console.log(resposta1, resposta2, resposta3);
+
+//colocando o texto da pergunta em variável
+pergunta1 = 'Você bebeu água hoje?';
+pergunta2 = 'Você comeu pão hoje?';
+pergunta3 = 'Sua camisa é vermelha?';
+
+//colocando a pergunta como variável no prompt
+resposta1 = prompt(pergunta1);
+resposta2 = prompt(pergunta2);
+resposta3 = prompt(pergunta3);
+
+//imprimindo a pergunta junto das respostas
+console.log(pergunta1, resposta1);
+console.log(pergunta2, resposta2);
+console.log(pergunta3, resposta3);
+
